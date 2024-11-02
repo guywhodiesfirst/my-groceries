1838df021b1b3e897a9f208a912c576afbd66481
6200211 <159926855+6200211@users.noreply.github.com>
2024-11-02 16:52:10 +0000

    Second Commit (new functions)

--- back-end/routes/product_routes.py
+++ back-end/routes/product_routes.py
@@ -1,3 +1,5 @@
+import re
+
 from bson import ObjectId
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
@@ -20,7 +22,7 @@ def getProducts():
 def getInformById():
     from app import mongo
 
-    productId = request.json.get('productId')
+    productId = request.json.get('product id')
     if not productId:
         return jsonify({"error": "ID продукту не надано"}), 400
 
@@ -34,3 +36,74 @@ def getInformById():
         return jsonify(product), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@productRoutes.route('/product/getCategories', methods=['GET'])
+def getCategories():
+    from app import mongo
+    try:
+        categories = []
+        categoriesCursor = mongo.db.products.find({}, {'category': 1})
+
+        for product in categoriesCursor:
+            category = product.get('category', [])
+            if category not in categories:
+                categories.append(category)
+
+        return jsonify({'categories': categories, 'message': 'Пошук пройшов успішно'}), 200
+    except Exception as e:
+        return jsonify({'error': 'Помилка пошуку категорій', 'details': str(e)}), 500
+
+
+@productRoutes.route('/product/getProdbyCat', methods=['GET'])
+@jwt_required()
+def getProductsByCategory():
+    from app import mongo
+
+    category = request.json.get('category')
+    if not category:
+        return jsonify({'message': 'Категорію не вказано'}), 400
+
+    try:
+        productsCursor = mongo.db.products.find({'category': category})
+        products = [{"_id": str(product["_id"]), "name": product.get("name", "No name"),"description": product.get("description", "No description"),
+                     "quantity": product.get("quantity", "0"),"price": product.get("price", "No price")} for product in productsCursor]
+
+        if not  products:
+            return jsonify({'message': 'Продуктів у цій категорії не знайдено'}), 404
+    except Exception as e:
+        return jsonify({'error': 'Помилка при отриманні продуктів', 'details': str(e)}), 500
+
+
+@productRoutes.route('/product/getProdbyName', methods=['GET'])
+@jwt_required()
+def getProductsByName():
+    from app import mongo
+
+    nameSubstring = request.json.get('name')
+    if not nameSubstring:
+        return jsonify({'message': 'Потрібно вказати частину імені продукту'}), 400
+
+    try:
+
+        productsCursor = mongo.db.products.find(
+            {'name': {'$regex': re.escape(nameSubstring), '$options': 'i'}}
+        )
+
+        products = [
+            {
+                "_id": str(product["_id"]),
+                "name": product.get("name", "No name"),
+                "description": product.get("description", "No description"),
+                "quantity": product.get("quantity", "0"),
+                "price": product.get("price", "No price")
+            }
+            for product in productsCursor
+        ]
+
+        if products:
+            return jsonify({'products': products, 'message': 'Продукти знайдено'}), 200
+        else:
+            return jsonify({'message': 'Продукти не знайдено'}), 404
+    except Exception as e:
+        return jsonify({'error': 'Помилка при отриманні продуктів', 'details': str(e)}), 500
+
