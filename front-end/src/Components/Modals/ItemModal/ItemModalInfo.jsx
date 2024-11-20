5ae9c047cf5a94033a0cdb2ecc0202a801c11563
JessFreak <maliy.olexander@lll.kpi.ua>
2024-11-20 12:06:15 +0200

    connected main page with backend

--- front-end/src/Components/Modals/ItemModal/ItemModalInfo.jsx
+++ front-end/src/Components/Modals/ItemModal/ItemModalInfo.jsx
@@ -4,11 +4,11 @@ import AddToCartButton from '../../AddToCartButton/AddToCartButton';
 export default function ItemModalInfo({itemData}) {
     return(
         <div className='modal--item-info'>
-            <p><span className='bold noto-sans'>Title: </span>{itemData.title}</p>
+            <p><span className='bold noto-sans'>Title: </span>{itemData.name}</p>
             <p><span className='bold noto-sans'>Description: </span>{itemData.description}</p>
             <p><span className='bold noto-sans'>Category: </span>{itemData.category}</p>
             <p><span className='bold noto-sans'>Price: </span>{itemData.price} UAH</p>
-            <p><span className='bold noto-sans'>Left in stock: </span>{itemData.unitsLeft}</p>
+            <p><span className='bold noto-sans'>Left in stock: </span>{itemData.quantity}</p>
             <AddToCartButton itemData={itemData} />
         </div>
     )
